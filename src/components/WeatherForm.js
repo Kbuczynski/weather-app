94c16716b8dabc0946974852efd9e17a5939ca89
Kbuczynski <kamil.buczynski00@interia.pl>
2020-02-13 12:30:25 +0100

    update package.json, bugs fix

--- src/components/WeatherForm.js
+++ src/components/WeatherForm.js
@@ -5,21 +5,20 @@ const WeatherForm = props => {
   const { handleInput, cityName, addCity, sortByTemperature, sortByCity } = props;
   
   return ( 
-    <Navbar bg="light" expand="lg">
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Navbar.Brand>Weather App</Navbar.Brand>
-
-      <Navbar.Collapse id="basic-navbar-nav">
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
         <Form inline onSubmit={addCity}>
           <FormControl type="text" required autoFocus placeholder="Enter city name" className="mr-sm-2" value={cityName} onChange={handleInput}/>
-          <Button variant="outline-dark" type="submit" className="mr-sm-2">Add</Button>
+          <Button variant="outline-light" type="submit" className="mr-sm-2">Add</Button>
         </Form>
-
-        <DropdownButton id="dropdown-basic-button" title="Sort by city" variant="outline-dark" className="mr-sm-2"> 
+        <DropdownButton id="dropdown-basic-button" title="Sort by city" variant="outline-light" className="mr-sm-2"> 
           <Dropdown.Item onClick={sortByCity} href="asc">A-Z</Dropdown.Item>
           <Dropdown.Item onClick={sortByCity} href="desc">Z-A</Dropdown.Item>
         </DropdownButton>
 
-        <DropdownButton id="dropdown-basic-button" title="Sort by temperature" variant="outline-dark">
+        <DropdownButton id="dropdown-basic-button" title="Sort by temperature" variant="outline-light">
           <Dropdown.Item onClick={sortByTemperature} href="asc">Ascending</Dropdown.Item>
           <Dropdown.Item onClick={sortByTemperature} href="desc">Descending</Dropdown.Item>
         </DropdownButton>
