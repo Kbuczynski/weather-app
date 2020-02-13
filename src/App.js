f6627e68b161f25d6eddce7179da681da9a98f3b
Kbuczynski <kamil.buczynski00@interia.pl>
2020-02-13 11:54:10 +0100

    adding sorting

--- src/App.js
+++ src/App.js
@@ -64,10 +64,10 @@ class App extends Component {
   getData = async () => {
     const { cityName } = this.state;
     const { addToData, isRepeats, handleError, closeError } = this;
-
     const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`;
+      
     const response = await fetch(API_URL);
-        
+       
     if (response.ok) {
       const cityData = await response.json();
   
@@ -77,6 +77,79 @@ class App extends Component {
       } else handleError();
 
     } else handleError();
+  }
+
+  sortByTemperature = e => {
+    e.preventDefault();
+
+    const method = e.target.href.split("/")[e.target.href.split("/").length - 1];
+
+    this.setState(state => {
+      const data = state.data.sort((a, b) => {
+        const tempA = a.main.temp;
+        const tempB = b.main.temp;
+
+        return this.comparison(method, tempA, tempB);
+      });
+
+      return {
+        data
+      };
+    });
+  }
+
+  sortByCity = e => {
+    e.preventDefault();
+
+    const method = e.target.href.split("/")[e.target.href.split("/").length - 1];
+
+    this.setState(state => {
+      const data = state.data.sort((a, b) => {
+        const cityA = a.name.toUpperCase();
+        const cityB = b.name.toUpperCase();
+        
+        return this.comparison(method, cityA, cityB);
+      });
+
+      return {
+        data
+      };
+    });
+  }
+
+  comparison = (method, tempA, tempB) => {
+    let comparison = 0;
+
+    if (method === "asc")
+      comparison = this.ascendingComparison(tempA, tempB);
+    else if (method === "desc")
+      comparison = this.descendingComparison(tempA, tempB);
+
+    return comparison;
+  }
+
+  ascendingComparison = (a, b) => {
+    const itemA = a;
+    const itemB = b;
+
+    let comparison = 0;
+
+    if (itemA > itemB) comparison = 1;
+    else if (itemA < itemB) comparison = -1;
+
+    return comparison;
+  }
+
+  descendingComparison = (a, b) => {
+    const itemA = a;
+    const itemB = b;
+
+    let comparison = 0;
+
+    if (itemA > itemB) comparison = -1;
+    else if (itemA < itemB) comparison = 1;
+
+    return comparison;
   }
 
   addToData = city => {
@@ -101,11 +174,11 @@ class App extends Component {
 
 	render() {
     const { cityName, data, error } = this.state;
-    const { handleInput, addCity, deleteCity, closeError } = this;
+    const { handleInput, addCity, deleteCity, closeError, sortByTemperature, sortByCity } = this;
 
 		return (
 			<>
-				<WeatherForm handleInput={handleInput} cityName={cityName} addCity={addCity} />
+				<WeatherForm handleInput={handleInput} cityName={cityName} addCity={addCity} sortByTemperature={sortByTemperature} sortByCity={sortByCity}/>
         {error && <ErrorMessage closeError={closeError} />}
 				<WeatherInfo data={data} deleteCity={deleteCity} />
 			</>
--- src/components/WeatherForm.js
+++ src/components/WeatherForm.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Navbar, Form, FormControl, Button } from 'react-bootstrap';
+import { Navbar, Form, FormControl, Button, DropdownButton, Dropdown } from 'react-bootstrap';
 
 const WeatherForm = props => {
-  const { handleInput, cityName, addCity } = props;
+  const { handleInput, cityName, addCity, sortByTemperature, sortByCity } = props;
   
   return ( 
     <Navbar bg="light" expand="lg">
@@ -10,9 +10,19 @@ const WeatherForm = props => {
 
       <Navbar.Collapse id="basic-navbar-nav">
         <Form inline onSubmit={addCity}>
-          <FormControl type="text" autoFocus placeholder="Enter city name" className="mr-sm-2" value={cityName} onChange={handleInput}/>
-          <Button variant="outline-dark" type="submit">Add</Button>
+          <FormControl type="text" required autoFocus placeholder="Enter city name" className="mr-sm-2" value={cityName} onChange={handleInput}/>
+          <Button variant="outline-dark" type="submit" className="mr-sm-2">Add</Button>
         </Form>
+
+        <DropdownButton id="dropdown-basic-button" title="Sort by city" variant="outline-dark" className="mr-sm-2"> 
+          <Dropdown.Item onClick={sortByCity} href="asc">A-Z</Dropdown.Item>
+          <Dropdown.Item onClick={sortByCity} href="desc">Z-A</Dropdown.Item>
+        </DropdownButton>
+
+        <DropdownButton id="dropdown-basic-button" title="Sort by temperature" variant="outline-dark">
+          <Dropdown.Item onClick={sortByTemperature} href="asc">Ascending</Dropdown.Item>
+          <Dropdown.Item onClick={sortByTemperature} href="desc">Descending</Dropdown.Item>
+        </DropdownButton>
       </Navbar.Collapse>
     </Navbar>
   );
