912e436502e09cb0fb9010e93f33e38d064ed38a
Kbuczynski <kamil.buczynski00@interia.pl>
2020-02-13 12:00:07 +0100

    Updates

---
+++ precache-manifest.d4dd9cc92d8842a3d376b07eef1a4300.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82095db4192dfdda4d48df9cddaea539",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "84e42b9a1a891efbc66a",
+    "url": "/weather-app/static/css/2.86aa6515.chunk.css"
+  },
+  {
+    "revision": "84e42b9a1a891efbc66a",
+    "url": "/weather-app/static/js/2.95b69f2e.chunk.js"
+  },
+  {
+    "revision": "d4438e5c6c63a1566cb0",
+    "url": "/weather-app/static/js/main.3fd58b56.chunk.js"
+  },
+  {
+    "revision": "52387862e54d9be38456",
+    "url": "/weather-app/static/js/runtime~main.68dbe4ab.js"
+  }
+]);
